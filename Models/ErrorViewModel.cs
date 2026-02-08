d9ff6b9394eeac503508f6b124fa511b34460763
MarioJey <marios.yaceg@gmail.com>
2026-02-08 14:47:41 +0100

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClassicLibraryMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

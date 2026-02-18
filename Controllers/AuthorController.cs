6bbe44234df064f3d0327285c1fdc2ad746c82df
MarioJey <marios.yaceg@gmail.com>
2026-02-18 12:51:58 +0100

    Update COntrollers

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -47,5 +47,82 @@ namespace ClassicLibraryMVC.Controllers
 
             return View(author);
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+
+            if(author == null)
+            {
+                return NotFound();
+            }
+
+            return View(author);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Author author)
+        {
+            if (id != author.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(author);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AuthorExists(author.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+        }
+
+        private bool AuthorExists(int id)
+        {
+            var author = _context.Authors.Find(id);
+            return author != null;
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+
+            if(author == null)
+            {
+                return NotFound();
+            }
+
+            return View(author);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+
+            if(author == null)
+            {
+                return NotFound();
+            }
+
+            _context.Authors.Remove(author);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- Controllers/PublishingHouseController.cs
+++ Controllers/PublishingHouseController.cs
@@ -47,6 +47,54 @@ namespace ClassicLibraryMVC.Controllers
 
             return View(publishingHouse);
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var publishingHouse = await _context.PublishingHouses.FindAsync(id);
+
+            if (publishingHouse == null)
+            {
+                return NotFound();
+            }
+
+            return View(publishingHouse);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, PublishingHouse publishingHouse)
+        {
+            if (id != publishingHouse.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(publishingHouse);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PublishingHouseExists(publishingHouse.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publishingHouse);
+        }
+
+        private bool PublishingHouseExists(int id)
+        {
+            return _context.PublishingHouses.Any(e => e.Id == id);
+        }
     }
 }
 
